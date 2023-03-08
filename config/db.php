7753415a6a2266ee18c36672b16467edec702b8e
abdul-aziz <abdulaziz.eu1@gmail.com>
2023-03-08 09:37:10 +0600

    work properly

--- config/db.php
+++ config/db.php
@@ -7,7 +7,7 @@
 
     $con = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
-    // Check connection
+    // connection Check
     if (mysqli_connect_errno()) {
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
         exit();
